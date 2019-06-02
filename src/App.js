620f4934c2b762f031b0edb17cf83f4669f7af05
Oelderoth <GMZues@gmail.com>
2019-06-02 16:56:10 -0500

    Use React Router Links on Sidebar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import BinaryBasics from 'components/BinaryBasics';
 
 import SideBar from 'components/Sidebar';
 import Music from 'components/Music';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import './App.scss';
 

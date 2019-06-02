f0d0103751013aea384317b74e0cecbf835aaec8
Rachael Metcalf <36243521+raespark@users.noreply.github.com>
2019-06-02 16:55:41 -0500

    binary basics content (#6)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import './App.scss';
+
 import Home from 'components/HomePage';
+import BinaryBasics from 'components/BinaryBasics';
+
 import SideBar from 'components/Sidebar';
 import Music from 'components/Music';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import './App.scss';
+
 
 function App() {
   return (
@@ -12,6 +16,7 @@ function App() {
       <SideBar />
       <div className="App">
         <Route exact path="/" component={Home} />
+        <Route exact path="/binary" component={BinaryBasics}/>
       </div>
     </Router>
   );

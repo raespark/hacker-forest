bd9aa9e4a26194631583a6f92fcdcf4e1114342e
Rachael Metcalf <raespark.games@gmail.com>
2019-06-02 23:21:32 -0500

    update site title and add favicon

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import './index.css';
+import Helmet from 'react-helmet';
+
 import App from './App';
 import {timerStart} from 'actions';
+
+import './index.css';
 
 import initializeStore from './InitializeStore';
 
@@ -13,5 +16,6 @@ store.dispatch(timerStart());
 ReactDOM.render(
     <Provider store={store}>
         <App />
+        <Helmet title="hacker-forest"/>
     </Provider>, document.getElementById('root')
 );
